8d2ef05d1b79f05041d267da87e4c84052521df1
shadurin <igor.shadurin@gmail.com>
2019-07-01 18:30:02 +0300

    store win section

--- server/daemon.js
+++ server/daemon.js
@@ -8,20 +8,7 @@ console.log('Server address: ' + serverAccountAddress);
 
 const signGame = (gameId, seed) => {
     const bytes = wc.base58Decode(gameId);
-    //const bytes = crypto.stringToBytes(gameId);
-    const gameIdBase58 = wc.base58Encode(bytes);
-    //const rand = Uint8Array.from("1".repeat(64).split('').map(Number));
-    //const signBase58 = wc.crypto().signBytes(seed, bytes, rand);
-    const signBase58 = crypto.signBytes(seed, bytes);
-    /*const isCorrect = crypto.verifySignature('DrYgfD7j6AKD1iXtn9uhMka95ATZB42B1kGKCUATP2UG', bytes, signBase58);
-    console.log('Game id 58', gameIdBase58);
-    console.log('Is correct', isCorrect);*/
-
-
-    return {
-        gameIdBase58,
-        signBase58
-    };
+    return crypto.signBytes(seed, bytes);
 };
 
 /*const sendSignedGame = (dappAddress, gameIdBase58, signBase58, seed) => {
@@ -55,7 +42,7 @@ const signGame = (gameId, seed) => {
         });
 
 };*/
-const bet = (dappAddress, expectedResult, seed) => {
+const bet = (dappAddress, expectedResult, amount, seed) => {
     const pubKey = crypto.publicKey(seed);
 
     const txData = invokeScript({
@@ -69,7 +56,7 @@ const bet = (dappAddress, expectedResult, seed) => {
             ]
         },
         payment: [
-            {amount: 1110000, asset: null}
+            {amount: amount, asset: null}
         ],
         chainId: 'T',
         senderPublicKey: pubKey
@@ -118,11 +105,11 @@ const result = (dappAddress, gameId, rsa, seed) => {
 
 };
 
-const gameId = '6ksrfB7Hr5w8i1jKD32fbnYV17Jp7QoAvoTBd8ZQYNkj';
-const data = signGame(gameId, serverAccountSeed);
-console.log('Sign is', data.signBase58);
+const gameId = 'Cj7R9FrJD17K56EHxQFAjsmc8EhKxSxWyx1oHv8p716y';
+const sign = signGame(gameId, serverAccountSeed);
+console.log('Sign is', sign);
 /*sendSignedGame(dappAddress, data.gameIdBase58, data.signBase58, serverAccountSeed);*/
 //const r=crypto.base58Decode(crypto.sha256(data.signBase58));
 //console.log(Number(r));
-//bet(dappAddress, 1, serverAccountSeed);
-result(dappAddress, gameId, data.signBase58, serverAccountSeed);
+bet(dappAddress, 1, 100500000, serverAccountSeed);
+//result(dappAddress, gameId, sign, serverAccountSeed);
