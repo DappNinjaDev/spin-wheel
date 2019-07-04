f5bf308024fe4b3d1cd810c657c66c5f9d9944a7
shadurin <igor.shadurin@gmail.com>
2019-07-04 14:55:06 +0300

    test script upgraded

--- test/test.js
+++ test/test.js
@@ -3,6 +3,9 @@ const {invokeScript, broadcast, waitForTx} = require('@waves/waves-transactions'
 const wc = require('@waves/waves-crypto');
 const crypto = wc.crypto();
 const fetch = require('node-fetch');
+const delay = require('delay');
+
+// todo add testnet/mainnet mode
 
 console.log('Dapp: ' + dappAddress);
 console.log('Server address: ' + serverAccountAddress);
@@ -12,80 +15,40 @@ const signGame = (gameId, seed) => {
     return crypto.signBytes(seed, bytes);
 };
 
-/*const sendSignedGame = (dappAddress, gameIdBase58, signBase58, seed) => {
+const bet = async (dappAddress, expectedResult, amount, seed) => {
     const pubKey = crypto.publicKey(seed);
 
     const txData = invokeScript({
         dApp: dappAddress,
         call: {
-            function: "GenerateRandInt",
+            function: "bet",
             args: [
                 {
-                    type: "string", value: gameIdBase58
-                },
-                {
-                    type: "string", value: signBase58
-                },
+                    type: "integer", value: expectedResult
+                }
             ]
         },
-        payment: [],
+        payment: [
+            {amount: amount, asset: null}
+        ],
         chainId: 'T',
         senderPublicKey: pubKey
     }, seed);
 
-    broadcast(txData, 'https://testnodes.wavesnodes.com')
-        .then(resp => {
-            console.log(resp);
+    return broadcast(txData, 'https://testnodes.wavesnodes.com')
+        .then(tx => {
+            console.log('Bet tx: ' + tx.id + '. Mining..');
+            return tx;
+        })
+        .then(tx => waitForTx(tx.id, {apiBase: 'https://testnodes.wavesnodes.com'}))
+        .then(result => {
+            console.log('Bet is ready!');
+            return result.id;
         })
         .catch(error => {
-            console.log('Error: ' + error.message);
+            console.log('Error bet: ' + error.message);
             //console.log(error);
         });
-
-};*/
-const bet = (dappAddress, expectedResult, amount, seed) => {
-    return new Promise((resolve, reject) => {
-        const pubKey = crypto.publicKey(seed);
-
-        const txData = invokeScript({
-            dApp: dappAddress,
-            call: {
-                function: "bet",
-                args: [
-                    {
-                        type: "integer", value: expectedResult
-                    }
-                ]
-            },
-            payment: [
-                {amount: amount, asset: null}
-            ],
-            chainId: 'T',
-            senderPublicKey: pubKey
-        }, seed);
-
-        return broadcast(txData, 'https://testnodes.wavesnodes.com')
-            .then(tx => {
-                //console.log(tx);
-                console.log('tx id:', tx.id);
-                /*
-                return waitForTx(tx.id)
-                    .then(result => {
-                        console.log('result is');
-                        console.log(result);
-                        resolve(result);
-                    })
-                    .catch(reject);*/
-                setTimeout(_ => {
-                    resolve(tx.id);
-                }, 10000)
-            })
-            .catch(error => {
-                console.log('Error: ' + error.message);
-                //console.log(error);
-                reject(error);
-            });
-    });
 };
 
 const result = (dappAddress, gameId, rsa, seed) => {
@@ -110,37 +73,35 @@ const result = (dappAddress, gameId, rsa, seed) => {
     }, seed);
 
     return broadcast(txData, 'https://testnodes.wavesnodes.com')
-        .then(resp => {
-            console.log(resp);
-            return resp;
+        .then(tx => {
+            console.log('Result tx: ' + tx.id + '. Mining..');
+            return tx;
+        })
+        .then(tx => waitForTx(tx.id, {apiBase: 'https://testnodes.wavesnodes.com'}))
+        .then(result => {
+            console.log('Result is ready!');
+            return result.id;
         })
         .catch(error => {
-            console.log('Error: ' + error.message);
+            console.log('Error result: ' + error.message);
             //console.log(error);
         });
 };
 
-
-/*sendSignedGame(dappAddress, data.gameIdBase58, data.signBase58, serverAccountSeed);*/
-//const r=crypto.base58Decode(crypto.sha256(data.signBase58));
-//console.log(Number(r));
-bet(dappAddress, 2, 100500000, serverAccountSeed)
-    .then((txId) => {
-        //const gameId = '21hVN9jMTvtS5Hmft9YoL1ch8uKgNKvwL8WeSpFAmqNi';
-        const sign = signGame(txId, serverAccountSeed);
-        console.log('Sign is', sign);
-        result(dappAddress, txId, sign, serverAccountSeed)
-            .then(async _ => {
-                // todo check https://testnodes.wavesnodes.com/addresses/data/3N8M61KCm8G72mK8PjacFbnnxdbFsyqQDeT/HPiQq6cq4Q5VKiUTwrcb1v6AxZcorRwPC1wWbai26gec_STATUS
-                for (let i = 0; i <= 500; i++) {
-                    const response = await fetch(`https://testnodes.wavesnodes.com/addresses/data/${dappAddress}/${txId}_STATUS`);
-                    const json = await response.json();
-                    console.log(json);
-                    if (json.value !== "NEW") {
-                        console.log(json.value);
-                        return;
-                    }
-                }
-            });
-    });
-
+(async () => {
+    const betResult = await bet(dappAddress, 2, 100500000, serverAccountSeed);
+    const sign = signGame(betResult, serverAccountSeed);
+    console.log('Sign is', sign);
+    await result(dappAddress, betResult, sign, serverAccountSeed);
+    for (let i = 0; i <= 100; i++) {
+        const response = await fetch(`https://testnodes.wavesnodes.com/addresses/data/${dappAddress}/${betResult}_STATUS`);
+        const json = await response.json();
+        console.log(json);
+        if (json.value && json.value !== "NEW") {
+            console.log(json.value);
+            return;
+        } else {
+            await delay(1000);
+        }
+    }
+})();
