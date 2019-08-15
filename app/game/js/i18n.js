f9ede6d7ee43f03deda2e28a128e06050e03d8cb
shadurin <igor.shadurin@gmail.com>
2019-08-15 14:21:59 +0300

    added russian translation

--- app/game/js/i18n.js
+++ app/game/js/i18n.js
@@ -7,7 +7,47 @@ const KEY_FAIL = 'fail';
 
 const LANG_EN = 'en';
 const LANG_RU = 'ru';
-let i18nDefaultLanguage = LANG_EN;
+const langs = [LANG_EN, LANG_RU];
+const getFirstBrowserLanguage = () => {
+    let result = null;
+    var nav = window.navigator,
+        browserLanguagePropertyKeys = ['language', 'browserLanguage', 'systemLanguage', 'userLanguage'],
+        i,
+        language;
+
+    // support for HTML 5.1 "navigator.languages"
+    if (Array.isArray(nav.languages)) {
+        for (i = 0; i < nav.languages.length; i++) {
+            language = nav.languages[i];
+            if (language && language.length) {
+                result = language;
+                break;
+            }
+        }
+    }
+
+    // support for other well known properties in browsers
+    for (i = 0; i < browserLanguagePropertyKeys.length; i++) {
+        language = nav[browserLanguagePropertyKeys[i]];
+        if (language && language.length) {
+            result = language;
+            break;
+        }
+    }
+
+    if (result) {
+        result = result.toLowerCase();
+        const split = result.split('-');
+        if (split.length >= 2) {
+            result = split[0];
+        }
+    }
+
+    return result;
+};
+
+//let i18nDefaultLanguage = langs.includes(getFirstBrowserLanguage()) ? getFirstBrowserLanguage() : LANG_EN;
+let i18nDefaultLanguage = LANG_RU;
 
 const textValues = {
     [LANG_EN]: {
@@ -18,11 +58,17 @@ const textValues = {
         [KEY_WIN_WAVES]: 'Win {count} WAVES',
         [KEY_FAIL]: 'Luck next time',
     },
-    [LANG_RU]: {},
+    [LANG_RU]: {
+        [KEY_SPIN_YOUR_FORTUNE]: 'Сделайте ставку',
+        [KEY_INSTRUCTION_BET_AND_SPIN]: 'Сделайте ставку и вращайте колесо',
+        [KEY_BALANCE]: 'Баланс: {balance}',
+        [KEY_SPINNING]: 'Вращаем...',
+        [KEY_WIN_WAVES]: 'Выиграно {count} WAVES',
+        [KEY_FAIL]: 'Попробуйте еще',
+    },
 };
 
 const getText = (key, data = {}, lang = i18nDefaultLanguage) => {
-
     let result = textValues[lang][key] ? textValues[lang][key] : key;
     Object.keys(data).forEach(key => {
         result = result.replace(`{${key}}`, data[key]);
